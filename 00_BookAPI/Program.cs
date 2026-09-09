d23db720f4c7a20da63f51b8dcf3b5766ad7552e
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2026-09-09 11:16:17 +0530

    refactor: move data modeling and storage to Data/

--- 00_BookAPI/Program.cs
+++ 00_BookAPI/Program.cs
@@ -1,14 +1,10 @@
+using _00_BookAPI.Data;
 using _00_BookAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IBookRepository, InMemoryBookRepository>();
 
 var app = builder.Build();
-
-var books = new List<Book> {
-    new Book(1, "Paper Towns", "John Green", 2008),
-    new Book(2, "Six of Crows", "Leigh Bardugo", 2015)
-};
 
 app.MapGet("/", () => "Hello World!");
 
@@ -43,17 +39,6 @@ app.MapPut("/books/{id}", (int id, Book book, IBookRepository repository) =>
 
 });
 
-// TEMPORARY ENDPOINT TO CREATE TOY DATA
-app.MapGet("/books/CREATE_ALL", (IBookRepository repository) =>
-{
-    foreach (var book in books)
-    {
-        repository.Add(book);
-    }
-
-    return Results.Redirect("/books");
-});
-
 app.MapDelete("/books/{id}", (int id, IBookRepository repository) =>
 {
     bool deleted = repository.Delete(id);
@@ -64,62 +49,3 @@ app.MapDelete("/books/{id}", (int id, IBookRepository repository) =>
 });
 
 app.Run();
-
-public interface IBookRepository
-{
-    IEnumerable<Book> GetAll();
-    Book? GetById(int id);
-    Book Add(Book book);
-    bool Update(int id, Book book);
-    bool Delete(int id);
-}
-
-public class InMemoryBookRepository : IBookRepository
-{
-
-    private readonly List<Book> _books = new();
-
-    public IEnumerable<Book> GetAll()
-    {
-        return _books;
-    }
-
-    public Book? GetById(int id)
-    {
-        return _books.FirstOrDefault(book => book.Id == id);
-    }
-
-    public Book Add(Book book)
-    {
-        int newId = _books.Count == 0
-            ? 1
-            : _books.Max(book => book.Id) + 1;
-
-        var newBook = new Book(newId, book.Title, book.Author, book.PublishedYear);
-        _books.Add(newBook);
-        return newBook;
-    }
-
-    public bool Update(int id, Book book)
-    {
-        var old_book = _books.FirstOrDefault(book => book.Id == id);
-
-        if (old_book is null)
-        {
-            return false;
-        }
-
-        old_book.Title = book.Title;
-        old_book.Author = book.Author;
-        old_book.PublishedYear = book.PublishedYear;
-        return true;
-    }
-
-    public bool Delete(int id)
-    {
-        var book = _books.FirstOrDefault(book => book.Id == id);
-        if (book is null) return false;
-        _books.Remove(book);
-        return true;
-    }
-}
